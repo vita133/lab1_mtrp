8a76147ed003b5005deff12bcf50a529ab2f7157
vita133 <vitandrukhiv@gmail.com>
2023-03-03 14:00:35 +0200

    optimize fun solveQuadEquation

--- Main.kt
+++ Main.kt
@@ -14,19 +14,21 @@ fun main(){
     println("End")
 }
 
-fun solveQuadEquation(a: Double, b: Double, c: Double){
+fun solveQuadEquation(a: Double, b: Double, c: Double) {
     val discriminant = b * b - 4 * a * c
-    if(discriminant > 0) {
-        val x1 = (-b + sqrt(discriminant)) / (2 * a)
-        val x2 = (-b - sqrt(discriminant)) / (2 * a)
-        println("Tow real roots: x1 = $x1 x2 = $x2")
-    }
-    else if (discriminant.toInt() == 0) {
-        val x = (-b + sqrt(discriminant)) / (2 * a)
-        println("One real root: x = $x")
-    }
-    else {
-        println("No real roots")
+    when {
+        discriminant > 0 -> {
+            val x1 = (-b + sqrt(discriminant)) / (2 * a)
+            val x2 = (-b - sqrt(discriminant)) / (2 * a)
+            println("Tow real roots: x1 = $x1 x2 = $x2")
+        }
+        discriminant == 0.0 -> {
+            val x = -b / (2 * a)
+            println("One real root: x = $x")
+        }
+        else -> {
+            println("No real roots")
+        }
     }
     return
 }
